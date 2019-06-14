e331f5a1c13df1d9e5bc2bba8a9a50dfd9b6b2ba
hyc554 <1415940604@qq.com>
2019-06-15 00:57:39 +0800

    Initial commit

---
+++ twisted-intro/twisted-deferred/defer-9.py
@@ -0,0 +1,32 @@
+import sys
+
+from twisted.internet.defer import Deferred
+
+
+def got_poem(poem):
+    print(poem)
+
+
+def poem_failed(err):
+    print('poem download failed', file=sys.stderr)
+    print('I am terribly sorry', file=sys.stderr)
+    print('try again later?', file=sys.stderr)
+
+
+def poem_done(_):
+    from twisted.internet import reactor
+    print("something wrong ...")
+    reactor.stop()
+
+
+d = Deferred()
+
+d.addCallbacks(got_poem, poem_failed)
+d.addBoth(poem_done)
+
+from twisted.internet import reactor
+
+# reactor.callWhenRunning(d.callback, 'Another short poem.')
+reactor.callWhenRunning(d.errback, Exception('First error'))
+
+reactor.run()
